4967c7a132772e82ac339cab100e1dd3e54cd753
Miguel Espinoza <miguel.espinozaleon@gmail.com>
2021-01-21 19:05:40 -0700

    idk commit

--- app/config/database.js
+++ app/config/database.js
@@ -7,7 +7,7 @@ module.exports = {
     connection: null,
     connect: function(){
         if(this.connection) return this.connection;
-        return mongoose.connect(process.env.DB).then(
+        return mongoose.connect(process.env.DB_URL).then(
             connection =>{
                 this.connection = connection;
                 console.log('Conexion a la base de datos exitosa');
