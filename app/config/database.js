36eebcdaafc58bb1702d70b9455a9722dae8e286
Miguel Espinoza <miguel.espinozaleon@gmail.com>
2021-01-21 21:43:42 -0700

    ty

--- app/config/database.js
+++ app/config/database.js
@@ -2,22 +2,30 @@ const mongoose = require('mongoose');
 const CONFIG = require('./config');
 
 require('dotenv').config({path: '.../variables.env'});
+const URI = process.env.DB_URL
+            ? process.env.DB_URL
+            : 'mongodb://localhost/HOTELCASINO';
 
 module.exports = {
     connection: null,
     connect: function(){
-        if(this.connection) return this.connection;
-        return mongoose.connect(process.env.DB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true
-        }).then(
-            connection =>{
-                this.connection = connection;
-                console.log('Conexion a la base de datos exitosa');
-            }).catch(error => console.log(error));
+
         
-    
+
+        mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+        });
+
+const connection = mongoose.connection;
+connection.on('error', console.error.bind(console, 'connection error'));
+connection.once('open', () => {
+    console.log('DB is conected');
+})
+
+
+        
     }
 
 }
