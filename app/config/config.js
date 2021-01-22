cdbc34e7f8936c895b03490f6eaadc1d996c97dc
Miguel Espinoza <miguel.espinozaleon@gmail.com>
2021-01-21 18:13:36 -0700

    third commit

--- app/config/config.js
+++ app/config/config.js
@@ -4,7 +4,9 @@
 require('dotenv').config({path:'.../variables.env'});
 module.exports = {
 PORT: process.env.PORT || 3000,
-DB: process.env.DB || 'mongodb://localhost:27017/platillos',
+
+DB: process.env.DB || 'mongodb+srv://miguelespinoza:El16330520@cluster0.oejvq.mongodb.net/platillos?retryWrites=true&w=majority',
+//DB: process.env.DB || 'mongodb://localhost:27017/platillos',
 HOST: 'localhost'
 
 }
--- app/config/database.js
+++ app/config/database.js
@@ -7,7 +7,7 @@ module.exports = {
     connection: null,
     connect: function(){
         if(this.connection) return this.connection;
-        return mongoose.connect(process.env.DB).then(
+        return mongoose.connect(CONFIG.DB).then(
             connection =>{
                 this.connection = connection;
                 console.log('Conexion a la base de datos exitosa');
