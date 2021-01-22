7c18e6ee84f3f3e3d4070a78eb1840219d906144
Miguel Espinoza <miguel.espinozaleon@gmail.com>
2021-01-21 21:30:15 -0700

    t

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ Database.connect();
 const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 3000;
 
-App.listen(port, host, function(error){
+App.listen(port, function(error){
     if(error) return console.log(error);
     console.log(`Servidor corriendo en el puerto ${port} y en el host ${host} `);
     
