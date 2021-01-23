0c22a25eec306b4ffd8da6dbe847a5944a27bfd3
Miguel Espinoza <miguel.espinozaleon@gmail.com>
2021-01-22 17:21:03 -0700

    lul

--- app/config/database.js
+++ app/config/database.js
@@ -1,24 +1,24 @@
 const mongoose = require('mongoose');
+const CONFIG = require('./config');
+const path = require('path');
+require('dotenv').config({path: path.resolve(__dirname, '.../variables.env')});
 
+module.exports = {
+    connection: null,
+    connect: function(){
+        if(this.connection) return this.connection;
+        try {
+            const connection = mongoose.connect(process.env.DB_URL);
+            this.connection = connection;
+            console.log('Conexion a la base de datos exitosa');
+        } catch (error) {
+            return console.log(error);
+        }
+        
+    
+    }
 
-const URI = process.env.DB_URL 
-            ? process.env.DB_URL
-            : 'mongodb://localhost/HOTELCASINO';
-
-mongoose.connect(URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-});
-
-const connection = mongoose.connection;
-connection.on('error', console.error.bind(console, 'connection error'));
-connection.once('open', () => {
-    console.log('DB is conected');
-})
-
-
-
+}
 
 
 
--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const App = require('./app/app');
 require('./app/config/database');
 require('dotenv').config({path: './variables.env'});
 
-//Database.connect();
+Database.connect();
 
 const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 3000;
 
 App.listen(port, function(error){
     if(error) return console.log(error);
-    console.log(`Servidor corriendo en el puerto ${port} y en el host ${host} `);
+    console.log(`Servidor corriendo en el puerto ${port} y en el host ${host}  en la base de datos ${process.env.DB_URL}`);
     
 });
